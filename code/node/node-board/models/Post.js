be4a819525e1136be8d7575d722e199d47c8267a
Jetty2020 <kmclo@naver.com>
2021-02-07 23:14:34 +0900

    upload

---
+++ code/node/node-board/models/Post.js
@@ -0,0 +1,16 @@
+// models/Post.js
+
+var mongoose = require('mongoose');
+
+// schema
+var postSchema = mongoose.Schema({ // 1
+  title:{type:String, required:[true,'Title is required!']}, // 1
+  body:{type:String, required:[true,'Body is required!']},   // 1
+  author:{type:mongoose.Schema.Types.ObjectId, ref:'user', required:true}, // 1
+  createdAt:{type:Date, default:Date.now}, // 2
+  updatedAt:{type:Date},
+});
+
+// model & export
+var Post = mongoose.model('post', postSchema);
+module.exports = Post;
